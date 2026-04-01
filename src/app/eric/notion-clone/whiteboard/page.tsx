9cac77a65bf688df5827a9d80533ff3b805da065
ericNotion <170554770+ericNotion@users.noreply.github.com>
2026-04-01 14:34:34 -0700

    fix: remove broken Agentation import from whiteboard page
    The import referenced "@/components/playground-kit/Agentation" (uppercase)
    but the file is "agentation.tsx" (lowercase), causing build failure on
    case-sensitive systems (Vercel/Linux). Additionally, Agentation doesn't
    accept children so it was being used incorrectly as a wrapper.

--- src/app/eric/notion-clone/whiteboard/page.tsx
+++ src/app/eric/notion-clone/whiteboard/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { Agentation } from "@/components/playground-kit/Agentation";
 import { WhiteboardCanvas } from "../components/whiteboard/WhiteboardCanvas";
 import { WhiteboardToolbar } from "../components/whiteboard/WhiteboardToolbar";
 import { ZoomControls } from "../components/whiteboard/ZoomControls";
 
 export default function WhiteboardPage() {
   return (
-    <Agentation>
-      <div className="relative w-full h-full">
-        <WhiteboardCanvas />
-        <WhiteboardToolbar />
-        <ZoomControls />
-      </div>
-    </Agentation>
+    <div className="relative w-full h-full">
+      <WhiteboardCanvas />
+      <WhiteboardToolbar />
+      <ZoomControls />
+    </div>
   );
 }
